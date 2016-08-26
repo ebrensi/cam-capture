7c0b4f34faef57b9ebedefc2112d8cbf5940b34c
Efrem <BarefootEfrem@gmail.com>
2016-08-25 17:57:22 -0700

    updated with bootstrap

--- app.py
+++ app.py
@@ -16,28 +16,6 @@ app.config.from_object(__name__)
 # Compress(app)
 
 
-def get_db():
-    db = getattr(g, "_database", None)
-    if db is None:
-        engine = create_engine(SQLALCHEMY_DATABASE_URI)
-        db = g._database = engine.connect()
-    return db
-
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
